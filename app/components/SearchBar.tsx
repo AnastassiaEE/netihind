e7d4afd1d02f995ff45da8ade12db809a0844744
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2023-12-17 19:14:06 +0200

    Add SearchBar component's placeholder parameter's type

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,19 +1,19 @@
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import Input from "./Input";
 
 
-export default function SearchBar({placeholder, handleChange, inputValue, children}) {
+export default function SearchBar({placeholder, handleChange, inputValue, children}: {placeholder: string, handleChange: Function, inputValue: string, children: ReactNode}) {
 
     const [isFocused, setIsFocused] = useState<boolean>(false);
 
-    const onFocus = (e: FocusEvent) => {
+    const onFocus = () => {
         setIsFocused(!isFocused);
         console.log(isFocused);
     }
 
     return (
         <>
-            <Input placeholder={placeholder} handleCh={handleChange} inputV={inputValue} focus={onFocus}/>
+            <Input placeholder={placeholder} handleChange={handleChange} inputValue={inputValue} focus={onFocus}/>
             <ul style={{height: '200px', overflow: 'scroll'}}>
                 {children}
             </ul>
