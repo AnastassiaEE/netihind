344e62dd029418ca46a346ffca769cc69924fe85
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2023-12-17 18:39:55 +0200

    add Button component parameters' types

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,6 +1,7 @@
+import { ReactNode } from 'react';
 import styles from '../styles/Button.module.css';
 
-export default function Button({children, active}) {
+export default function Button({children, active}:{children: ReactNode, active: boolean}) {
 
     return (
         <button className={styles.btn} disabled={!active}> {children} </button>
