977331b34a040225805fbeffd2357e3d6c7a1bcd
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-01-23 06:02:17 +0200

    Improved info section image.

--- app/components/InfoSection.tsx
+++ app/components/InfoSection.tsx
@@ -6,7 +6,7 @@ export default function InfoSection() {
             bg="bg-white"> 
             <div className="container">
                 <h2 className="text-4xl font-extrabold text-center mb-10"> Rhoncus est pellentesque </h2>
-                <div className="flex items-center">
+                <div className="flex items-center justify-between">
                     <div className="md:w-6/12">
                             <div className="text-slate-600 text-base">
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
@@ -17,8 +17,8 @@ export default function InfoSection() {
                                 <p>Ut faucibus pulvinar elementum integer enim. Euismod lacinia at quis risus sed vulputate.</p>
                             </div>
                     </div>
-                    <div className="md:w-6/12 flex justify-end">
-                        <div className="relative w-5/6">
+                    <div className="md:w-5/12">
+                        <div className="relative">
                             <img src="/man.png" alt="" className="relative z-10 w-full"/>
                             <div className="bg-gradient-to-br from-indigo-500/40 from-10% via-violet-500/40 to-fuchsia-500/40 to-60% rounded-3xl absolute bottom-0 left-1/2 -translate-x-1/2 w-[90%] h-[95%]">
                                 <div className="absolute rounded-full bg-red-500 top-20 w-3 h-3"></div>
