a01552930731eb17cf5ea535e4cbfdf8bef1447b
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-01-28 22:53:12 +0200

    Added svg with animation to top section.

--- app/components/TopSection.tsx
+++ app/components/TopSection.tsx
@@ -1,11 +1,25 @@
 import AddressForm from './AddressForm';
+import MeshSvg from './MeshSvg';
 
 export default function TopSection() {
     return(
-        <section className="h-screen flex items-center bg-gradient-to-tl from-indigo-500/10 from-0% via-violet-500/10 via-20% via-fuchsia-500/10 to-70%">
+        <section className="h-screen max-md:flex max-md:items-center md:relative bg-gradient-to-tl from-indigo-500/10 from-0% via-violet-500/10 via-20% via-fuchsia-500/10 to-70%">
+            <MeshSvg>
+                <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%" > 
+                    <stop offset="0%" stop-color="#a5b4fc">
+                        <animate attributeName="stop-color" values="#a5b4fc; #ddd6fe; #f5d0fe; #a5b4fc" dur="4s" repeatCount="indefinite"></animate>
+                    </stop>
+                    <stop offset="50%" stop-color="#ddd6fe">
+                        <animate attributeName="stop-color" values="#ddd6fe; #f5d0fe; #a5b4fc; #ddd6fe" dur="4s" repeatCount="indefinite"></animate>
+                    </stop>
+                    <stop offset="100%" stop-color="#f5d0fe">
+                        <animate attributeName="stop-color" values="#f5d0fe; #a5b4fc; #ddd6fe; #f5d0fe" dur="4s" repeatCount="indefinite"></animate>
+                    </stop>
+                </linearGradient> 
+            </MeshSvg>
             <div className="container">
-                <div className="flex">
-                    <div className="w-12/12">
+                <div className="md:absolute md:top-1/4">
+                    <div className="md:w-9/12">
                         <h1 className="text-5xl leading-snug font-extrabold mb-4"> Поиск <span className="bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-violet-500 to-fuchsia-500">провайдеров</span> домашнего интернета по адресу </h1>
                         <p className="text-lg text-slate-600 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi non arcu risus quis varius quam quisque id diam.</p>
                         <AddressForm/>
