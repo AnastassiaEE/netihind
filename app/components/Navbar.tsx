a7aa3bc49ee1dc08df67976068cfdd0280b36bef
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-07-02 22:00:02 +0300

    Added logo to navbar.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,29 @@
+import Image from 'next/image'
+import mainLogo from '../../public/images/gradientmainlogo.svg';
+import Link from 'next/link';
+
+
 export default function Navbar() {
     return (
-        <header > 
-            <div className="container">
-
+        <header className="py-2"> 
+            <div className="container px-4">
+                <div className="flex">
+                    <Link href="/" className="">
+                        <Image
+                            className="w-40"
+                            src={mainLogo}
+                            width={500}
+                            height={500}
+                            alt="navbar logo"/>
+                    </Link>   
+                    <div>
+                        <ul className="flex">
+                            <li>Link 1</li>
+                            <li>Link 2</li>
+                            <li>Link 3</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </header>
     )
