da885d19d6d6d2f9739b9bc47fcf58a55b1c6e6e
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-07-06 18:15:11 +0300

    Fixed typescript errors in AddressFortmatter.

--- app/utils/addressFormatter.tsx
+++ app/utils/addressFormatter.tsx
@@ -2,14 +2,14 @@ const addressRegex = /^[^\d]*[\d\/A-Za-z]*$/g;
 const apartmentRegex = /(?<=\-).*$/g;
 
 export const getAddress = (address: string) => {
-    return address.match(addressRegex)?.[0];
+    return address.match(addressRegex)?.[0] ?? '';
 }
 
 export const getApartment = (address: string) => {
-    return address.match(apartmentRegex)?.[0];
+    return address.match(apartmentRegex)?.[0] ?? '';
 }
 
-export const removeExtraChars = (address: string | undefined) => {
+export const removeExtraChars = (address: string) => {
     return address?.replace('tn ', '').replace(/[A-Za-z]{1}\.\s/g, '');
 }
 
