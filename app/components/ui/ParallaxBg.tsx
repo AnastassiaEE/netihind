d4c9e20aa36bedb3527b6a8768dec2bf210601d5
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-08-13 20:59:33 +0300

    Created parallax bg component.

---
+++ app/components/ui/ParallaxBg.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { ParallaxBanner, ParallaxBannerLayer, ParallaxProvider } from 'react-scroll-parallax';
+import { StaticImageData } from "next/image"
+
+
+export default function ParallaxBg({img}: {img: StaticImageData}) {
+    return (
+        <ParallaxProvider>
+            <ParallaxBanner style={{ aspectRatio: '2 / 1', height: '36.45vw', minHeight: '300px' }}>
+                <ParallaxBannerLayer 
+                    image={img.blurDataURL}
+                    speed={-20} />
+            </ParallaxBanner>  
+        </ParallaxProvider>
+      );
+}
