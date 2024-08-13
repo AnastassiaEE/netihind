07f284fbae19484dee4003406c9453fe14570388
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-08-13 20:29:56 +0300

    Removed unnecessary home page Main component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
+import QuestionsSection from "./components/sections/QuestionsSection";
+import ContactsSection from "./components/sections/ContactsSection";
+import StepsSection from "./components/sections/StepsSection";
+import BlogSection from "./components/sections/BlogSection";
+import InfoSection from "./components/sections/InfoSection";
+import TopSection from "./components/sections/TopSection";
 import PageLayout from './layouts/PageLayout';
-import Main from './Main';
 
 export default function Page() {
   return (
     <PageLayout headerVariant="primary">
-      <Main/>
+      <TopSection/>
+      <InfoSection/>
+      <StepsSection/>
+      <QuestionsSection/>
+      <BlogSection/>
+      <ContactsSection/>
     </PageLayout>
   )
 }
