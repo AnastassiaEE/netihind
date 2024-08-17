56249933b7ccf7665b0228f467db20cdcba572fb
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-08-17 17:30:50 +0300

    Created blog and blog post pages. Added some dummy data for posts.

--- app/data/posts.ts
+++ app/data/posts.ts
@@ -1,10 +1,46 @@
 import computer from "../../public/images/computer.jpg"
 
 const posts = [
-    {title: 'Iaculis nunc sed augue lacus1.', image: computer, alt: 'Computer with coffee', href: '', date: '05.04.2022'},
-    {title: 'Iaculis nunc sed augue lacus2.', image: computer, alt: 'Computer with coffee', href: '', date: '05.04.2022'},
-    {title: 'Iaculis nunc sed augue lacus3.', image: computer, alt: 'Computer with coffee', href: '', date: '05.04.2022'},
-    {title: 'Iaculis nunc sed augue lacus4.', image: computer, alt: 'Computer with coffee', href: '', date: '05.04.2022'},
+    {
+      id: '1a', title: 'Iaculis nunc sed augue lacus1.', image: computer, alt: 'Computer with coffee', href: '', date: '05.04.2022', 
+      content: 
+      `# Welcome to my MDX page!
+      This is some **bold** and _italics_ text.
+      This is a list in markdown:
+      - One
+      - Two
+      - Three`
+    },
+    {
+      id: '2a', title: 'Iaculis nunc sed augue lacus2.', image: computer, alt: 'Computer with coffee', href: '', date: '05.04.2022', 
+      content: 
+      `# Welcome to my MDX page!
+      This is some **bold** and _italics_ text.
+      This is a list in markdown:
+      - One
+      - Two
+      - Three`
+    },
+    {
+      id: '3a', title: 'Iaculis nunc sed augue lacus3.', image: computer, alt: 'Computer with coffee', href: '', date: '05.04.2022', 
+      content: 
+      `# Welcome to my MDX page!
+      This is some **bold** and _italics_ text.
+      This is a list in markdown:
+      - One
+      - Two
+      - Three`
+    },
+    {
+      id: '4a', title: 'Iaculis nunc sed augue lacus4.', image: computer, alt: 'Computer with coffee', href: '', date: '05.04.2022', 
+      content: 
+      `# Welcome to my MDX page!
+      This is some **bold** and _italics_ text.
+      This is a list in markdown:
+      - One
+      - Two
+      - Three`
+    },
   ]
 
 export default posts
