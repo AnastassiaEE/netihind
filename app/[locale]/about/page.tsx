7208bfbe5abc24d6f50f5010dc46b019f3d872f1
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-09-16 01:14:53 +0300

    Display mdx content (ru/et) on the about page. Added translations.

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,10 +1,25 @@
+import initTranslations from "@/app/i18n";
 import AboutSection from "@/components/sections/AboutSection";
+import TranslationsProvider from "@/components/TranslationProvider";
 import PageLayout from "@/layouts/PageLayout";
 
-export default function Page() {
+const i18nNamespaces = ['about', 'navigation', 'form'];
+
+export default async function About({
+    params: { locale } 
+}: {
+    params: { locale: string }
+}) {
+    const { t, resources } = await initTranslations(locale, i18nNamespaces);
+
     return (
-        <PageLayout>
-            <AboutSection/>
-        </PageLayout>
+        <TranslationsProvider
+            namespaces={i18nNamespaces}
+            locale={locale}
+            resources={resources}>
+            <PageLayout>
+                <AboutSection locale={locale}/>
+            </PageLayout>
+        </TranslationsProvider>
     )
 }
--- app/components/sections/AboutSection.tsx
+++ app/components/sections/AboutSection.tsx
@@ -1,9 +1,22 @@
-import SectionLayout from "@/layouts/SectionLayout";
+'use client'
 
-export default function functionAboutSection() {
+import MdxLayout from "@/layouts/MdxLayout";
+import SectionLayout from "@/layouts/SectionLayout";
+import dynamic from "next/dynamic";
+import { useTranslation } from "react-i18next";
+
+export default function AboutSection({locale}: {locale: string}) {
+    const { t } = useTranslation('about');
+    const Loading = () => <p>Loading</p>;
+    const AboutMdxEt = dynamic(() => import(''+'@/markdown/et/about.mdx'), {loading: () => <Loading/>}) 
+    const AboutMdxRu = dynamic(() => import(''+'@/markdown/ru/about.mdx'), {loading: () => <Loading/>}) 
     return (
         <SectionLayout className="p-24">
-            <h1 className="text-4xl font-extrabold mb-10">About Us</h1>
+            <h1 className="text-4xl font-extrabold mb-10">{t('title')}</h1>
+            <MdxLayout>
+                {locale === 'et' && <AboutMdxEt/>}
+                {locale === 'ru' && <AboutMdxRu/>}
+            </MdxLayout>
         </SectionLayout>
     )
 }
