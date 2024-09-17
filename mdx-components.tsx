493e147c3384cb16a2c6a658d80f33fe979804f1
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-09-17 20:45:19 +0300

    Added global mdx li tag style.

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,9 @@
-import type { MDXComponents } from 'mdx/types'
+import type { MDXComponents } from 'mdx/types';
+import Check from '@/components/ui/icons/Check';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    li: ({ children }) => <li><Check size="small" />{children}</li>,
     ...components,
-  }
+  };
 }
