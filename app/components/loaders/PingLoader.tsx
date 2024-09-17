085ec863f930e037376fc925b72e20827ac5ac4b
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-09-17 17:40:16 +0300

    Added loader for about page content

---
+++ app/components/loaders/PingLoader.tsx
@@ -0,0 +1,7 @@
+export default function PingLoader() {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <div className="rounded-full h-20 w-20 bg-primary animate-ping"></div>
+        </div>
+    ) 
+}
--- app/components/sections/AboutSection.tsx
+++ app/components/sections/AboutSection.tsx
@@ -4,12 +4,12 @@ import MdxLayout from "@/layouts/MdxLayout";
 import SectionLayout from "@/layouts/SectionLayout";
 import dynamic from "next/dynamic";
 import { useTranslation } from "react-i18next";
+import PingLoader from "../loaders/PingLoader";
 
 export default function AboutSection({locale}: {locale: string}) {
     const { t } = useTranslation('about');
-    const Loading = () => <p>Loading</p>;
-    const AboutMdxEt = dynamic(() => import(''+'@/markdown/et/about.mdx'), {loading: () => <Loading/>}) 
-    const AboutMdxRu = dynamic(() => import(''+'@/markdown/ru/about.mdx'), {loading: () => <Loading/>}) 
+    const AboutMdxEt = dynamic(() => import(''+'@/markdown/et/about.mdx'), {loading: () => <PingLoader/>}) 
+    const AboutMdxRu = dynamic(() => import(''+'@/markdown/ru/about.mdx'), {loading: () => <PingLoader/>}) 
     return (
         <SectionLayout>
             <h1 className="text-4xl font-extrabold mb-10">{t('title')}</h1>
