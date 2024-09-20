62b87e88f3283def80e6582771ad8b82558a1069
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-09-20 21:41:53 +0300

    Defined global mdx styles instead of using tailwind typography.

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -3,7 +3,42 @@ import Check from '@/components/ui/icons/Check';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    li: ({ children }) => <li><Check size="small" />{children}</li>,
+    h1: ({ children }) => (
+      <h1 className="text-[calc(1.375rem+1.5vw)] md:text-4xl font-extrabold mb-10">{children}</h1>
+    ),
+    h2: ({ children }) => <h2 className="text-2xl font-extrabold mb-4 mt-0">{children}</h2>,
+    p: ({ children }) => (
+      <p className=" text-muted-dark [&:not(:last-child)]:mb-6 mt-0"> {children} </p>
+    ),
+    ul: ({ children }) => <ul className="list-none [&:not(:last-child)]:mb-6">{children}</ul>,
+    ol: ({ children }) => <ol className="list-none [&:not(:last-child)]:mb-6"> {children}</ol>,
+    li: ({ children }) => (
+      <li className="text-muted-dark mb-1">
+        <Check size="small" />
+        {children}
+      </li>
+    ),
+    table: ({ children }) => (
+      <table className="table-auto border border-collapse border-muted-light text-muted-dark [&:not(:last-child)]:mb-6">
+        {children}
+      </table>
+    ),
+    th: ({ children }) => (
+      <th className="border border-muted-light p-20" style={{ padding: '10px' }}>
+        {children}
+      </th>
+    ),
+    td: ({ children }) => (
+      <td className="border border-muted-light" style={{ padding: '10px' }}>
+        {children}
+      </td>
+    ),
+    a: ({ children }) => (
+      <a href={`mailto:${children}`} className="hover:text-primary transition-colors font-semibold">
+        {children}
+      </a>
+    ),
+    strong: ({ children }) => <strong className="text-black font-bold">{children}</strong>,
     ...components,
   };
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
-import type { Config } from 'tailwindcss'
-import colors, { indigo } from "tailwindcss/colors"
+import type { Config } from 'tailwindcss';
+import colors from 'tailwindcss/colors';
 
 const config: Config = {
   content: [
@@ -7,31 +7,31 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {   
+  theme: {
     extend: {
       animation: {
         show: 'show .25s cubic-bezier(0.4, 0, 0.2, 1)',
-      }, 
+      },
       keyframes: {
         show: {
           '0%': { transform: 'translateY(-100%)' },
-          '100%': { transform: 'translateY(0)' }
-        }
+          '100%': { transform: 'translateY(0)' },
+        },
       },
       colors: {
         primary: colors.indigo['500'],
         'primary-dark': colors.indigo['600'],
         secondary: colors.violet['500'],
         accent: colors.fuchsia['500'],
-        
+
         neutral: colors.indigo['100'],
         'neutral-dark': colors.gray['900'],
         'neutral-light': colors.indigo['50'],
 
         muted: colors.gray['400'], // small text, icons, placeholders, bullets
         'muted-dark': colors.slate['600'], // base text, inputs, links
-        'muted-light': colors.slate['200'],// items borders
-       
+        'muted-light': colors.slate['200'], // items borders
+
         valid: colors.gray['300'],
         invalid: colors.red['500'],
 
@@ -43,23 +43,23 @@ const config: Config = {
         'instagram-logo-shadow': '#dc2743',
         'twitter-logo': '#333333',
         'youtube-logo': 'rgb(255, 0, 0)',
-        'linkedin-logo': '#0077B5'
+        'linkedin-logo': '#0077B5',
       },
 
       backgroundImage: {
-        'instagram-logo': 'linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)'
+        'instagram-logo':
+          'linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)',
       },
       gridTemplateColumns: {
-        'tariffs': 'repeat(5, minmax(max-content, 100%))'
-      }
+        tariffs: 'repeat(5, minmax(max-content, 100%))',
+      },
     },
   },
   corePlugins: {
-    container: false
+    container: false,
   },
   plugins: [
-    require('@tailwindcss/typography'),
-    function ({ addComponents } : {addComponents: any}) {
+    function ({ addComponents }: { addComponents: any }) {
       addComponents({
         '.container': {
           marginLeft: 'auto',
@@ -80,9 +80,9 @@ const config: Config = {
           '@screen 2xl': {
             maxWidth: '1320px',
           },
-        }
-      })
-    }
+        },
+      });
+    },
   ],
-}
-export default config
+};
+export default config;
