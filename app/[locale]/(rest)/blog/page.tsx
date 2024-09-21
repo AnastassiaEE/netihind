21530cc5224da367a99a44ed19800dd554cbc0b1
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-09-21 15:20:58 +0300

    Devided sections into separate folders.

--- app/[locale]/(rest)/blog/page.tsx
+++ app/[locale]/(rest)/blog/page.tsx
@@ -1,4 +1,4 @@
-import ListBlogSection from '@/components/sections/ListBlogSection';
+import ListBlogSection from '@/components/sections/blog/ListBlogSection';
 
 export default function Blog() {
     return <ListBlogSection />;
