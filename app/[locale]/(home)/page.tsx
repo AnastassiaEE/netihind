d76ec5d47071cacb46ddf15ef5c390e7f8b364d9
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-09-22 23:52:25 +0300

    Hide info section from the home page.

--- app/[locale]/(home)/page.tsx
+++ app/[locale]/(home)/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <>
       {/* <TopSectionPrimary /> */}
       <TopSectionSecondary />
-      <InfoSection />
+      {/* <InfoSection /> */}
       <StepsSection />
       <QuestionsSection />
       <SliderBlogSection />
