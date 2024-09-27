b556f8e124f7f8b8d21c2efe737f51249a41e6bd
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-09-27 09:49:53 +0300

    Bug fix.

--- app/components/sections/home/ProvidersLogoSection.tsx
+++ app/components/sections/home/ProvidersLogoSection.tsx
@@ -1,11 +1,11 @@
-import SliderLogoCards from '@/components/ui/logo/SLiderLogoCards';
+import SliderLogoCards from '@/components/ui/logo/SliderLogoCards'
 import providerLogos from '@/data/providerLogos';
 import SectionLayout from '@/layouts/SectionLayout';
 
 export default function ProvidersSection() {
     return (
         <SectionLayout className="pt-24">
-            <SliderLogoCards logos={providerLogos} />
+            <SliderLogoCards logos={providerLogos}></SliderLogoCards>
         </SectionLayout>
     );
 }
