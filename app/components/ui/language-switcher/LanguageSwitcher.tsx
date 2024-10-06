3e98af0b85cd409668d45939c471ae1f50fcfb22
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-10-06 03:34:46 +0300

    Added language cookie.

--- app/components/ui/language-switcher/LanguageSwitcher.tsx
+++ app/components/ui/language-switcher/LanguageSwitcher.tsx
@@ -5,6 +5,7 @@ import { usePathname } from 'next/navigation';
 import { useTranslation } from 'react-i18next';
 import i18nConfig from '@/i18nConfig';
 import Language from './Language';
+import { setCookie } from 'cookies-next';
 
 export default function LanguageSwitcher() {
   const { i18n } = useTranslation();
@@ -17,6 +18,13 @@ export default function LanguageSwitcher() {
   const handleClick = (e: React.MouseEvent<HTMLSpanElement>) => {
     e.preventDefault();
     const newLocale = (e.target as HTMLLinkElement).getAttribute('lang');
+
+    const days = 30;
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    const expires = date.toUTCString();
+    document.cookie = `NEXT_LOCALE=${newLocale};expires=${expires};path=/`;
+
     // redirect to the new locale path
     if (currentLocale === i18nConfig.defaultLocale && !i18nConfig.prefixDefault) {
       router.push('/' + newLocale + currentPathname);
