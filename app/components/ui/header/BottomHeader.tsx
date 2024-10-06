de7327f5560dea9c15e63bd96122baa04290b714
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-10-06 05:50:31 +0300

    Testing header nav items.

--- app/components/ui/header/BottomHeader.tsx
+++ app/components/ui/header/BottomHeader.tsx
@@ -12,6 +12,7 @@ export default function BottomHeader() {
     const { t, i18n } = useTranslation('navigation');
     const localePrefix = getLocalePrefix(i18n.language, i18nConfig.defaultLocale);
     const pathname = usePathname();
+    console.log(pathname)
 
     return (
         <div className="p-2 flex justify-between">
