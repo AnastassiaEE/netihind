6c9bb70c67d7ffe4266102c88e9e223c75a5d309
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-10-11 07:40:34 +0300

    Translated related posts button. Changed related posts button style.

--- app/components/sections/blog/RelatedBlogPostsSection.tsx
+++ app/components/sections/blog/RelatedBlogPostsSection.tsx
@@ -1,18 +1,20 @@
+'use client';
+
 import SectionLayout from '@/layouts/SectionLayout';
 import Button from '@/components/ui/form/buttons/Button';
 import Link from 'next/link';
+import { useTranslation } from 'react-i18next';
 
 export default function RelatedBlogPostsSection() {
+    const { t } = useTranslation('blog');
     return (
         <SectionLayout>
             <div className="flex justify-end">
-                <div className="w-full md:w-44">
-                    <Link href="/blog">
-                        <Button variant="secondary" size="lg">
-                            All Posts →
-                        </Button>
-                    </Link>
-                </div>
+                <Link href="/blog" className="w-full md:w-72">
+                    <Button variant="secondary" size="lg" className="w-full">
+                        {t('buttons.all-posts')}
+                    </Button>
+                </Link>
             </div>
         </SectionLayout>
     );
