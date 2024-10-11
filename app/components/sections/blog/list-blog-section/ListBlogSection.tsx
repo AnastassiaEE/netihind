23ca8a6c2a5856233910936780e76afaf86fb07d
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-10-11 15:34:06 +0300

    Refactored ListBlogSectionComponent.

--- app/components/sections/blog/list-blog-section/ListBlogSection.tsx
+++ app/components/sections/blog/list-blog-section/ListBlogSection.tsx
@@ -1,4 +1,4 @@
-import ListBlogSectionTitle from "@/components/sections/blog/ListBlogSectionTitle";
+import ListBlogSectionTitle from "@/components/sections/blog/list-blog-section/ListBlogSectionTitle";
 import BlogCardsWrapper from "@/components/ui/blog/BlogCardsWrapper";
 import ListBlogCards from "@/components/ui/blog/ListBlogCards";
 import SectionLayout from "@/layouts/SectionLayout";
