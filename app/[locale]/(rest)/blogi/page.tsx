2b3b7b832efa2eca1ac6d1df670ad0f493cdd19d
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-10-13 01:30:56 +0300

    Refactored list blog section code.

--- app/[locale]/(rest)/blogi/page.tsx
+++ app/[locale]/(rest)/blogi/page.tsx
@@ -1,5 +1,7 @@
-import ListBlogSection from '@/components/sections/blog/list-blog-section/ListBlogSection';
+import initTranslations from '@/app/i18n';
+import ListBlogSection from '@/components/sections/blog/ListBlogSection';
 
-export default function Blog({ params: { locale } }: { params: { locale: string } }) {
-    return <ListBlogSection locale={locale} />
+export default async function Blog({ params: { locale } }: { params: { locale: string } }) {
+    const { i18n } = await initTranslations(locale, ['blog']);
+    return <ListBlogSection i18n={i18n} />;
 }
--- app/components/sections/blog/ListBlogSection.tsx
+++ app/components/sections/blog/ListBlogSection.tsx
@@ -1,13 +1,14 @@
-import ListBlogSectionTitle from "@/components/sections/blog/list-blog-section/ListBlogSectionTitle";
 import BlogCardsWrapper from "@/components/ui/blog/BlogCardsWrapper";
 import ListBlogCards from "@/components/ui/blog/ListBlogCards";
 import SectionLayout from "@/layouts/SectionLayout";
+import { i18n } from "i18next";
+import { H1 } from "@/components/ui/headings/RestPageHeadings";
 
-export default function ListBlogSection({ locale }: { locale: string }) {
+export default function ListBlogSection({ i18n }: { i18n: i18n }) {
     return (
         <SectionLayout>
-            <ListBlogSectionTitle />
-            <BlogCardsWrapper locale={locale}>
+            <H1>{i18n.t('title')}</H1>
+            <BlogCardsWrapper i18n={i18n}>
                 <ListBlogCards />
             </BlogCardsWrapper>
         </SectionLayout >
