611b608bc22964584662ebb0e7d68d507f912f8c
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-10-27 09:16:26 +0200

    Test commit.

--- app/lib/wpFetch.ts
+++ app/lib/wpFetch.ts
@@ -3,10 +3,6 @@ const API_URL = process.env.NEXT_PUBLIC_WORDPRESS_API_ENDPOINT as string;
 export async function fetchAPI(query = '', { variables }: Record<string, any> = {}) {
   const headers: { [key: string]: string } = { 'Content-Type': 'application/json' };
 
-  if (process.env.WORDPRESS_AUTH_REFRESH_TOKEN) {
-    headers['Authorization'] = `Bearer ${process.env.WORDPRESS_AUTH_REFRESH_TOKEN}`;
-  }
-
   const res = await fetch(API_URL, {
     headers,
     method: 'POST',
