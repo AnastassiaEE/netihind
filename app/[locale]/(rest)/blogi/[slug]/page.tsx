4709857b80dd1286d3b4ff48de6a45516e15e51e
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-10-27 13:27:14 +0200

    Test commit.

--- app/[locale]/(rest)/blogi/[slug]/page.tsx
+++ app/[locale]/(rest)/blogi/[slug]/page.tsx
@@ -8,8 +8,6 @@ import { Suspense } from 'react';
 import PingLoader from '@/components/ui/loaders/PingLoader';
 import getFormattedSlug from '@/utils/slugFormatter';
 
-export const revalidate = 3600;
-
 export async function generateStaticParams() {
     // const response = await fetch(`${process.env.BASE_URL}/api/posts`, {
     //     method: 'POST',
