2415c69df9e499cb59824de633730ebe8b03199b
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-11-02 14:35:11 +0200

    1. Deleted the google address form. 2. Deleted the loader component.

--- app/components/ui/loader/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Loader.module.css';
-
-export default function Loader() {
-    return (
-        <div className="flex justify-center items-center absolute w-full h-full z-50 bg-primary">
-            <span className={styles.loader}></span>
-        </div>
-    );
-}
