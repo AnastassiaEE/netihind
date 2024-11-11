2ea761f335eacf9ea6ec9072819a2aab5bf32e20
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-11-11 14:04:18 +0200

    Translated the address page button filters.

--- app/components/ui/filter/ButtonsFilter.tsx
+++ app/components/ui/filter/ButtonsFilter.tsx
@@ -2,9 +2,13 @@
 
 import useButtonsFilter from '@/hooks/useButtonsFilter';
 import Button from '@/components/ui/form/buttons/Button';
+import { useMediaQuery } from '@mui/material';
+import { useTranslations } from 'next-intl';
 
 export default function ButtonsFilter({ filters }: { filters: { [key: string]: boolean } }) {
     const { usedFilters, handleFilterClick } = useButtonsFilter(filters);
+    const isSmallScreen = useMediaQuery('(max-width:540px)');
+    const t = useTranslations('AddressPage');
     return (
         <div className="flex flex-wrap gap-2">
             {Object.keys(usedFilters).map((filter) => (
@@ -12,11 +16,11 @@ export default function ButtonsFilter({ filters }: { filters: { [key: string]: b
                     key={filter}
                     handleClick={handleFilterClick}
                     variant={usedFilters[filter] ? 'primary' : 'secondary'}
-                    size="sm"
+                    size={isSmallScreen ? 'sm' : 'lg'}
                     className="!w-max rounded-md uppercase"
                     name={filter}
                 >
-                    {filter}
+                    {t(`filters.${filter}`)}
                 </Button>
             ))}
         </div>
