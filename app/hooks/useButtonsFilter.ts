d1f172168bfbc63a22b714b5db178c7ca154b188
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-11-13 19:09:19 +0200

    Changed the useButtonsFilter hook logic.

--- app/hooks/useButtonsFilter.ts
+++ app/hooks/useButtonsFilter.ts
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import { useRouter, usePathname } from '@/i18n/routing';
-import { useParams } from 'next/navigation';
+import { useParams, useSearchParams } from 'next/navigation';
 
 export default function useButtonsFilter(filters: { [key: string]: boolean }) {
   const [usedFilters, setUsedFilters] = useState(filters);
   const router = useRouter();
   const pathname = usePathname();
   const params = useParams();
+  const searchParams = new URLSearchParams(useSearchParams().toString());
+  console.log(searchParams);
 
   const activateFilterButton = (activeFilter: string) => {
     const filters = { ...usedFilters };
@@ -18,9 +20,11 @@ export default function useButtonsFilter(filters: { [key: string]: boolean }) {
 
   const handleFilterClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     const clickedFilter = (e.target as HTMLButtonElement).name;
+    searchParams.set('filter', clickedFilter);
+    const searchParamsObject = Object.fromEntries(searchParams.entries());
     router.replace(
       // @ts-expect-error
-      { pathname, params, query: { filter: clickedFilter } },
+      { pathname, params, query: searchParamsObject },
       { scroll: false },
     );
     activateFilterButton(clickedFilter);
