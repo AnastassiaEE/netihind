6c570c1f47d719d4f089ebe3f5f3eb989a8b3380
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-11-24 01:43:40 +0200

    Added a more accurate image alt.

--- app/components/ui/Logo.tsx
+++ app/components/ui/Logo.tsx
@@ -4,7 +4,7 @@ import { Link } from '@/i18n/routing';
 export default function Logo({ src, sizeClass }: { src: StaticImageData; sizeClass: string }) {
     return (
         <Link href="/">
-            <Image className={sizeClass} src={src} alt="logo" width={0} height={0} />
+            <Image className={sizeClass} src={src} alt="Netihind logo" width={0} height={0} />
         </Link>
     );
 }
