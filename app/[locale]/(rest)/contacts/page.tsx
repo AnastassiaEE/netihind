e3b30e4d3ab7a84499ed9a52d5e9a7d0d2800bd6
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-11-24 01:19:55 +0200

    Refactored the contacts page.

--- app/[locale]/(rest)/contacts/page.tsx
+++ app/[locale]/(rest)/contacts/page.tsx
@@ -1,41 +1,28 @@
 import ContactCards from '@/components/ui/contacts/ContactCards';
 import ContactForm from '@/components/ui/form/forms/ContactForm';
 import { H1, H2 } from '@/components/ui/headings/RestPageHeadings';
-import { email, phone } from '@/data/contacts';
 import SectionLayout from '@/layouts/SectionLayout';
 import { useTranslations } from 'next-intl';
 import { setRequestLocale } from 'next-intl/server';
+import { contacts } from '@/data/contacts';
 
 export default function Contacts({ params: { locale } }: { params: { locale: string } }) {
     setRequestLocale(locale);
     const t = useTranslations('ContactsPage');
-    const contacts: {
-        type: 'email' | 'phone' | 'address';
-        data: { title: string; description: string; contact: string };
-    }[] = [
-            {
-                type: 'email',
-                data: {
-                    title: t('cards.email.title'),
-                    description: t('cards.email.description'),
-                    contact: email,
-                },
-            },
-            {
-                type: 'phone',
-                data: {
-                    title: t('cards.phone.title'),
-                    description: t('cards.phone.description'),
-                    contact: phone,
-                },
-            },
-        ];
+
+    const contactsList = Object.keys(contacts).map((type) => ({
+        contactType: type as 'email' | 'phone' | 'address',
+        title: t(`cards.${type}.title`),
+        description: t(`cards.${type}.description`),
+        contact: contacts[type as keyof typeof contacts],
+    }));
+
     return (
         <SectionLayout>
             <H1>{t('title')}</H1>
             <div className="lg:flex justify-between items-center">
                 <div className="lg:w-6/12 max-lg:mb-24">
-                    <ContactCards contacts={contacts} />
+                    <ContactCards contacts={contactsList} />
                 </div>
                 <div className="lg:w-5/12 bg-neutral-light px-7 md:px-12 py-9 rounded-lg">
                     <H2>{t('formTitle')}</H2>
