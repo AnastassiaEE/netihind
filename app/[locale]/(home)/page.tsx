ea915581e20f8f4325cd07e82bf47eeb17265d2e
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-11-24 01:37:40 +0200

    Deleted the TopSectionSecondary component. Renamed the TopSectionPrimary component.

--- app/[locale]/(home)/page.tsx
+++ app/[locale]/(home)/page.tsx
@@ -3,9 +3,8 @@ import ContactsSection from '@/components/sections/home/ContactsSection';
 import StepsSection from '@/components/sections/home/StepsSection';
 import SliderBlogSection from '@/components/sections/home/SliderBlogSection';
 import InfoSection from '@/components/sections/home/InfoSection';
-import TopSectionSecondary from '@/components/sections/home/TopSectionSecondary';
 import ProvidersLogoSection from '@/components/sections/home/ProvidersLogoSection';
-import TopSectionPrimary from '@/components/sections/home/TopSectionPrimary';
+import TopSection from '@/components/sections/home/TopSection';
 import { getTranslations, setRequestLocale } from 'next-intl/server';
 import { useTranslations } from 'next-intl';
 import { formatISO } from 'date-fns';
@@ -73,8 +72,7 @@ export default function Home({ params: { locale } }: { params: { locale: string 
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
       />
-      <TopSectionPrimary />
-      {/* <TopSectionSecondary /> */}
+      <TopSection />
       {/* <InfoSection i18n={t} /> */}
       {/* <ProvidersLogoSection /> */}
       <StepsSection />
