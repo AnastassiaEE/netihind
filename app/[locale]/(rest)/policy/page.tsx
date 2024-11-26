7e89d1ded40d42296e1bd58d45ac956335ff2367
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-11-26 22:07:16 +0200

    Refactored the privacy policy page.

--- app/[locale]/(rest)/policy/page.tsx
+++ app/[locale]/(rest)/policy/page.tsx
@@ -6,7 +6,7 @@ import SectionLayout from '@/layouts/SectionLayout';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import components from '@/mdx-components';
 import { setRequestLocale } from 'next-intl/server';
-import PageSkeletonLoader from '@/components/ui/loaders/PageSkeletonLoader';
+import PageLoader from '@/components/ui/loaders/PageLoader';
 
 export const revalidate = 3600;
 
@@ -19,14 +19,12 @@ const options = {
 export default async function Policy({ params: { locale } }: { params: { locale: string } }) {
     setRequestLocale(locale);
     const page = await getPage(`policy-${locale}`);
-    if (page === undefined || page === null) {
-        notFound();
-    }
+    if (!page) notFound();
     return (
-        <SectionLayout>
-            <Suspense fallback={<PageSkeletonLoader />}>
+        <Suspense fallback={<PageLoader />}>
+            <SectionLayout>
                 <MDXRemote source={page.content} components={components as {}} options={options} />
-            </Suspense>
-        </SectionLayout>
+            </SectionLayout>
+        </Suspense>
     );
 }
