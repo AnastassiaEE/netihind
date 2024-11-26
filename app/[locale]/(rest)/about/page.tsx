0f849360ec095e6e487d88d49b257dbdc96639c6
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-11-26 22:02:04 +0200

    Refactored the about page.

--- app/[locale]/(rest)/about/page.tsx
+++ app/[locale]/(rest)/about/page.tsx
@@ -8,7 +8,7 @@ import { getTranslations, setRequestLocale } from 'next-intl/server';
 import { formatISO } from 'date-fns';
 import { openGraphLogo, readAction, website } from '@/app/shared-metadata';
 import { metadataBaseUrl } from '@/app/[locale]/layout';
-import PageSkeletonLoader from '@/components/ui/loaders/PageSkeletonLoader';
+import PageLoader from '@/components/ui/loaders/PageLoader';
 
 export const revalidate = 3600;
 
@@ -70,20 +70,19 @@ export default async function About({ params: { locale } }: { params: { locale: 
     };
 
     const page = await getPage(`about-${locale}`);
-    if (page === undefined || page === null) {
-        notFound();
-    }
+    if (!page) notFound();
+
     return (
         <>
             <script
                 type="application/ld+json"
                 dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
             />
-            <SectionLayout>
-                <Suspense fallback={<PageSkeletonLoader />}>
+            <Suspense fallback={<PageLoader />}>
+                <SectionLayout>
                     <MDXRemote source={page.content} components={components as {}} />
-                </Suspense>
-            </SectionLayout>
+                </SectionLayout>
+            </Suspense>
         </>
     );
 }
