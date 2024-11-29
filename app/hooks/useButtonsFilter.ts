f6d2f2479bd5ccbbe958b968797d2af080b63109
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2024-11-29 21:40:22 +0200

    Refactored the useButtonsFilter hook.

--- app/hooks/useButtonsFilter.ts
+++ app/hooks/useButtonsFilter.ts
@@ -8,25 +8,22 @@ export default function useButtonsFilter(filters: { [key: string]: boolean }) {
   const pathname = usePathname();
   const params = useParams();
 
-  const activateFilterButton = useCallback((filter: string) => {
+  const activateFilterButton = (filter: string) => {
     setUsedFilters((prevFilters) =>
       Object.fromEntries(
         Object.keys(prevFilters).map((prevFilter) => [prevFilter, prevFilter === filter]),
       ),
     );
-  }, []);
+  };
 
-  const handleFilterClick = useCallback(
-    (filter: string) => {
-      router.replace(
-        // @ts-expect-error
-        { pathname, params, query: { filter: filter } },
-        { scroll: false },
-      );
-      activateFilterButton(filter);
-    },
-    [router, pathname, params, activateFilterButton],
-  );
+  const handleFilterClick = (filter: string) => {
+    router.replace(
+      // @ts-expect-error
+      { pathname, params, query: { filter: filter } },
+      { scroll: false },
+    );
+    activateFilterButton(filter);
+  };
 
   return {
     usedFilters,
