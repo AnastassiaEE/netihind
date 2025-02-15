ef36f6ece9a4d4f0b1fa2b15e379abc4259da5d1
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-02-15 22:09:57 +0200

    Improved the sort component.

--- app/hooks/useSort.ts
+++ app/hooks/useSort.ts
@@ -6,7 +6,7 @@ export default function useSort(selected: string) {
   const router = useRouter();
   const pathname = usePathname();
   const params = useParams();
-  const searchParams = new URLSearchParams(useSearchParams().toString());
+  const searchParams = useSearchParams();
   const [selectedOption, setSelectedOption] = useState<string>(selected);
 
   useEffect(() => {
@@ -15,8 +15,9 @@ export default function useSort(selected: string) {
 
   const handleChange = (option: string) => {
     setSelectedOption(option);
-    searchParams.set('sort', option);
-    const searchParamsObject = Object.fromEntries(searchParams.entries());
+    const newSearchParams = new URLSearchParams(searchParams);
+    newSearchParams.set('sort', option);
+    const searchParamsObject = Object.fromEntries(newSearchParams.entries());
     router.replace(
       // @ts-expect-error
       { pathname, params, query: searchParamsObject },
