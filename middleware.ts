aefe607954bc737293d2f74b3f4bd69fc03f8816
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-02-15 23:02:29 +0200

    Changed the middleware file so that the trailing slash is not added after the query params.

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,33 @@
 import createMiddleware from 'next-intl/middleware';
 import { routing } from '@/i18n/routing';
+import { NextRequest, NextResponse } from 'next/server';
 
-export default createMiddleware(routing);
+export default async function middleware(request: NextRequest) {
+  const { searchParams } = request.nextUrl;
 
-// only applies this middleware to files in the app directory
+  const updatedSearchParams = new URLSearchParams();
+  let needsRedirect = false;
+
+  searchParams.forEach((value, key) => {
+    if (value.endsWith('/')) {
+      updatedSearchParams.set(key, value.slice(0, -1));
+      needsRedirect = true;
+    } else {
+      updatedSearchParams.set(key, value);
+    }
+  });
+
+  if (needsRedirect) {
+    request.nextUrl.search = updatedSearchParams.toString();
+    return NextResponse.redirect(request.nextUrl, 308);
+  }
+
+  const handleI18nRouting = createMiddleware(routing);
+  const response = handleI18nRouting(request);
+
+  return response;
+}
+
 export const config = {
-  matcher: ['/((?!api|static|.*\\..*|_next).*)'],
+  matcher: ['/', '/(et|ru)/:path*', '/((?!api|static|.*\\..*|_next).*)'],
 };
