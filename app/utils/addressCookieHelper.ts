46998d80ba30baf48bb2aff08c66341578b157e7
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-02-16 04:28:09 +0200

    Changed address cookie helper a bit.

--- app/utils/addressCookieHelper.ts
+++ app/utils/addressCookieHelper.ts
@@ -1,8 +1,8 @@
 export const getAddressCookieValues = (cookieString: string | undefined) => {
   const cookie = JSON.parse(cookieString || '{}');
-  const fullAddress = cookie.full || '';
-  const oid = cookie.oid || '';
-  const apartment = cookie.apartment || '';
+  const fullAddress = cookie.full ?? '';
+  const oid = cookie.oid ?? '';
+  const apartment = cookie.apartment ?? '';
   return {
     fullAddress,
     oid,
