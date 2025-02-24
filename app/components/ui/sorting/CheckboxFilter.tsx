cf93aae73cbe7bd197b86faee34243d3cddbfa13
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-02-24 18:33:01 +0200

    Moved checkboxfilter logic to the checkboxfiltersgroup hook.

--- app/components/ui/sorting/CheckboxFilter.tsx
+++ app/components/ui/sorting/CheckboxFilter.tsx
@@ -1,29 +1,24 @@
-'use client';
-
 import Checkbox from '@/components/ui/form/fields/Checkbox';
-import useCheckboxFilter from '@/hooks/useCheckboxFilter';
 
 export default function CheckboxFilter({
     name,
     options,
     selected,
+    handleChange,
 }: {
     name: string;
     options: { value: string; label: string }[];
     selected: { value: string; label: string }[];
+    handleChange: (name: string, value: string, checked: boolean) => void;
 }) {
-    const { selectedOptions, handleChange } = useCheckboxFilter(name, options, selected);
-
     return (
         <>
             {options.map((option) => (
                 <div key={option.value} className="py-0.5">
                     <Checkbox
                         name={name}
-                        isChecked={selectedOptions.some(
-                            (opt) => opt.value.toString() === option.value.toString(),
-                        )}
-                        handleChange={handleChange}
+                        isChecked={selected.some((opt) => opt.value.toString() === option.value.toString())}
+                        handleChange={(e) => handleChange(name, option.value, e.target.checked)}
                         value={option.value}
                     >
                         {option.label}
--- app/hooks/useCheckboxFiltersGroup.ts
+++ app/hooks/useCheckboxFiltersGroup.ts
@@ -1,37 +1,73 @@
 import { useParams, useSearchParams } from 'next/navigation';
 import { usePathname, useRouter } from '@/i18n/routing';
+import { useEffect, useState } from 'react';
+import { Filters } from '@/components/ui/sorting/CheckboxFiltersGroup';
 
-export default function useCheckboxFilterGroup(filters: {
-  [key: string]: {
-    options: {
-      value: string;
-      label: string;
-    }[];
-    selected: {
-      value: string;
-      label: string;
-    }[];
-  };
-}) {
+export default function useCheckboxFilterGroup(filters: Filters) {
   const router = useRouter();
   const pathname = usePathname();
   const params = useParams();
   const searchParams = useSearchParams();
 
-  const handleClear = () => {
-    const newSearchParams = new URLSearchParams(searchParams);
-    Object.keys(filters).forEach((filterKey) => {
-      newSearchParams.delete(filterKey);
+  const [selectedFilters, setSelectedFilters] = useState(filters);
+
+  useEffect(() => {
+    setSelectedFilters(filters);
+  }, [filters]);
+
+  useEffect(() => {
+    const updateUrlParams = (selectedFilters: Filters) => {
+      const newSearchParams = new URLSearchParams(searchParams);
+      Object.entries(selectedFilters).forEach(([name, filter]) => {
+        filter.selected.length > 0
+          ? newSearchParams.set(name, filter.selected.map((opt) => opt.label).join(','))
+          : newSearchParams.delete(name);
+      });
+      const searchParamsObject = Object.fromEntries(newSearchParams.entries());
+      router.replace(
+        // @ts-expect-error
+        { pathname, params, query: searchParamsObject },
+        { scroll: false },
+      );
+    };
+
+    updateUrlParams(selectedFilters);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedFilters]);
+
+  const handleChange = (name: string, value: string, checked: boolean) => {
+    setSelectedFilters((prevFilters) => {
+      const filter = prevFilters[name];
+      if (!filter) return prevFilters;
+
+      const selectedOptions = filter.options.find((opt) => opt.value === value);
+      if (!selectedOptions) return prevFilters;
+
+      const newSelectedOptions = checked
+        ? [...filter.selected, selectedOptions]
+        : filter.selected.filter((option) => option.value !== value);
+
+      return {
+        ...prevFilters,
+        [name]: { ...filter, selected: newSelectedOptions },
+      };
     });
-    const searchParamsObject = Object.fromEntries(newSearchParams.entries());
-    router.replace(
-      // @ts-expect-error
-      { pathname, params, query: searchParamsObject },
-      { scroll: false },
+  };
+
+  const handleClear = () => {
+    const clearedFilters = Object.keys(selectedFilters).reduce(
+      (acc: typeof selectedFilters, filterKey) => {
+        acc[filterKey] = { ...selectedFilters[filterKey], selected: [] };
+        return acc;
+      },
+      {},
     );
+    setSelectedFilters(clearedFilters);
   };
 
   return {
+    selectedFilters,
+    handleChange,
     handleClear,
   };
 }
