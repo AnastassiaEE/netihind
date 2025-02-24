ceee79b86276d88e26874fea792e6712378415c7
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-02-24 23:34:48 +0200

    Changed page loader styles.

--- app/components/ui/loaders/PageLoader.tsx
+++ app/components/ui/loaders/PageLoader.tsx
@@ -1,10 +1,10 @@
 export default function PageLoader() {
     return (
-        <div className="absolute inset-0 bg-white flex justify-center items-center">
+        <div className="fixed inset-0 bg-white flex justify-center items-center">
             <div className="relative inline-flex">
                 <div className="w-9 h-9 bg-primary rounded-full"></div>
-                <div className="w-9 h-9 bg-primary rounded-full absolute inset-0 animate-ping"></div>
-                <div className="w-9 h-9 bg-primary rounded-full absolute inset-0 animate-pulse"></div>
+                <div className="w-9 h-9 bg-primary rounded-full absolute animate-ping"></div>
+                <div className="w-9 h-9 bg-primary rounded-full absolute animate-pulse"></div>
             </div>
         </div>
     );
