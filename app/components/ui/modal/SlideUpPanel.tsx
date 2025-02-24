a9ca2f9561a6b41a666c68d29afee9f8473ba663
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-02-24 20:50:25 +0200

    Added some styles.

--- app/components/ui/modal/SlideUpPanel.tsx
+++ app/components/ui/modal/SlideUpPanel.tsx
@@ -22,7 +22,7 @@ export default function SlideUpPanel({
     const { handleTouchStart, handleTouchEnd } = useSlideUpPanel(handleClose);
 
     const panelClasses = classNames(
-        'fixed z-50 top-0 left-0 h-screen max-h-screen w-full bg-white rounded-t-2xl transition-transform',
+        'fixed z-50 top-0 left-0 right-0 h-screen bg-white rounded-t-2xl transition-transform',
         isOpened ? 'translate-y-0' : 'translate-y-full',
     );
 
