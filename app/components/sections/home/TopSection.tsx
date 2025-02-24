75dde056233c2a503426af7e41dc91798a20352e
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-02-24 23:21:55 +0200

    TopSection layout small change.

--- app/components/sections/home/TopSection.tsx
+++ app/components/sections/home/TopSection.tsx
@@ -10,7 +10,6 @@ import { useTranslations } from 'next-intl';
 const sectionClasses = classNames(
     'h-screen',
     'min-h-[650px]',
-    'md:h-screen',
     'md:min-h-[720px]',
     'pt-[80px]',
     'md:pt-[140px]',
