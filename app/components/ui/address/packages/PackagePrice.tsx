d3a1739130ff61058c0eec922ecb9048798c3ccb
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-02-28 20:44:36 +0200

    Changed tailwind classes order.

--- app/components/ui/address/packages/PackagePrice.tsx
+++ app/components/ui/address/packages/PackagePrice.tsx
@@ -9,16 +9,19 @@ export default function PackagePrice({
     promoPrice?: number;
 }) {
     const t = useTranslations('Packages');
+
     const priceClasses = classNames(
-        'text-lg md:text-xl tracking-tight rounded-lg w-max px-2 py-1',
+        'w-max rounded-lg px-2 py-1 text-lg tracking-tight md:text-xl',
         promoPrice
-            ? 'text-white bg-gradient-to-r from-primary via-secondary to-accent'
-            : 'text-muted-dark bg-primary-light',
+            ? 'bg-gradient-to-r from-primary via-secondary to-accent text-white'
+            : 'bg-primary-light text-muted-dark',
     );
+
     const price = promoPrice ?? originalPrice;
     const discountPercentage = promoPrice
         ? Math.ceil(((originalPrice - promoPrice) / promoPrice) * 100)
         : 0;
+
     return (
         <div className="max-lg:flex max-lg:items-center max-lg:gap-2">
             <p className={priceClasses}>
@@ -26,8 +29,8 @@ export default function PackagePrice({
             </p>
             {promoPrice && (
                 <div className="mt-1">
-                    <p className="text-black font-semibold inline mr-2">{originalPrice}€</p>
-                    <span className="text-sm font-medium text-transparent bg-clip-text bg-gradient-to-r from-primary via-secondary to-accent">
+                    <p className="mr-2 inline font-semibold text-black">{originalPrice}€</p>
+                    <span className="bg-gradient-to-r from-primary via-secondary to-accent bg-clip-text text-sm font-medium text-transparent">
                         -{discountPercentage}%
                     </span>
                 </div>
