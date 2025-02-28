eaee2fbc94bc3986ca895a8bcb60f0d135336659
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-02-28 20:41:53 +0200

    Changed tailwind classes order.

--- app/components/ui/address/packages/PackageIcon.tsx
+++ app/components/ui/address/packages/PackageIcon.tsx
@@ -2,7 +2,7 @@ import { SvgIconComponent } from '@mui/icons-material';
 
 export default function PackageIcon({ Icon }: { Icon: SvgIconComponent }) {
     return (
-        <div className="w-10 h-10 bg-primary rounded-md absolute shadow-md shadow-primary/50 max-md:right-5 max-md:-top-2 md:-left-5 flex justify-center items-center">
+        <div className="absolute flex size-10 items-center justify-center rounded-md bg-primary shadow-md shadow-primary/50 max-md:-top-2 max-md:right-5 md:-left-5">
             <Icon className="text-white" />
         </div>
     );
