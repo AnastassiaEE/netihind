0046e9d0e8feebab70118ab8e957f379bb17dcba
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-02-28 20:51:44 +0200

    Changed tailwind classes order.

--- app/components/ui/blog/PostDate.tsx
+++ app/components/ui/blog/PostDate.tsx
@@ -2,7 +2,7 @@ import getFormattedDate from '@/utils/dateFormatter';
 
 export default function PostDate({ date }: { date: string }) {
     return (
-        <time dateTime={date} className="text-muted text-sm">
+        <time dateTime={date} className="text-sm text-muted">
             {getFormattedDate(date)}
         </time>
     );
