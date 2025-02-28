6c03f338e42cd121f06f83b488619ef6c08e8520
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-02-28 20:28:38 +0200

    Changed tailwind classes order.

--- app/components/sections/home/TopSection.tsx
+++ app/components/sections/home/TopSection.tsx
@@ -1,5 +1,4 @@
 import SectionLayout from '@/layouts/SectionLayout';
-import classNames from 'classnames';
 import providerLogos from '@/data/providerLogos';
 import LogoCards from '@/components/ui/logo/LogoCards';
 import { H1 } from '@/components/ui/headings/HomePageHeadings';
@@ -7,46 +6,30 @@ import GradientMesh from '@/components/ui/mesh/GradientMesh';
 import MaaAmetAddressForm from '@/components/ui/form/forms/MaaAmetAddressForm';
 import { useTranslations } from 'next-intl';
 
-const sectionClasses = classNames(
-    'h-screen',
-    'min-h-[650px]',
-    'md:min-h-[720px]',
-    'pt-[80px]',
-    'md:pt-[140px]',
-    'flex',
-    'flex-col',
-    'justify-center',
-    'relative'
-);
-
-const bgClasses = classNames(
-    'bg-gradient-to-t',
-    'from-primary/20 from-0%',
-    'via-secondary/10 via-40%',
-    'via-accent/10 to-80%',
-);
 
 export default function TopSection() {
     const t = useTranslations('HomePage');
     return (
-        <SectionLayout className={sectionClasses} bg={bgClasses}>
-            <div className="absolute right-0 max-md:top-[80px] bottom-[50px] w-[230px] md:w-[500px] h-auto -z-10">
+        <SectionLayout
+            className="relative flex h-screen min-h-[650px] flex-col justify-center pt-[80px] md:min-h-[720px] md:pt-[140px]"
+            bg="bg-gradient-to-t from-primary/20 from-0% via-accent/10 via-secondary/10 via-40% to-80%">
+            <div className="absolute bottom-[50px] right-0 -z-10 h-auto w-[230px] max-md:top-[80px] md:w-[500px]">
                 <GradientMesh />
             </div>
             <div className="relative md:-translate-y-16">
                 <H1>
                     {t.rich('topSection.title', {
                         span: (chunks) => (
-                            <span className="bg-clip-text text-transparent bg-gradient-to-r from-primary via-secondary to-accent">
+                            <span className="bg-gradient-to-r from-primary via-secondary to-accent bg-clip-text text-transparent">
                                 {chunks}
                             </span>
                         ),
                     })}
                 </H1>
-                <p className="text-md md:text-lg mb-4">{t('topSection.description')}</p>
+                <p className="mb-4 md:text-lg">{t('topSection.description')}</p>
                 <MaaAmetAddressForm />
             </div>
-            <div className="absolute inset-x-0 bottom-0 py-3 md:p-9 bg-primary/10 -z-10">
+            <div className="absolute inset-x-0 bottom-0 -z-10 bg-primary/10 py-3 md:p-9">
                 <div className="md:container">
                     <LogoCards logos={providerLogos} />
                 </div>
