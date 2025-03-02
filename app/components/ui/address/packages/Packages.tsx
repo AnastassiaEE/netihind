df4dbebdd9eab50148a4091109aa4c9d8b84708e
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-03-02 07:34:02 +0200

    Enhance Packages component to display selected package details in modal

--- app/components/ui/address/packages/Packages.tsx
+++ app/components/ui/address/packages/Packages.tsx
@@ -8,7 +8,8 @@ import PackagesError from '@/components/ui/errors/PackagesError';
 import { Link } from '@/i18n/routing';
 import Modal from '@/components/ui/modal/Modal';
 import useOverlay from '@/hooks/useOverlay';
-import CloseButton from '../../buttons/CloseButton';
+import { useState } from 'react';
+import Image from 'next/image';
 
 export default function Packages({
     oid,
@@ -39,12 +40,15 @@ export default function Packages({
         isOverlayVisible: isModalOpened,
         openOverlay: openModal,
         closeOverlay: closeModal,
-        overlayRef: modalRef
+        overlayRef: modalRef,
     } = useOverlay();
 
-    const handlePackageButtonClick = () => {
+    const [selectedPackage, setSelectedPackage] = useState<{ [key: string]: any } | null>(null);
+
+    const handlePackageButtonClick = (packageData: { [key: string]: any }) => {
+        setSelectedPackage(packageData);
         openModal();
-    }
+    };
 
     const errorContent = (error: string) => (
         <PackagesError>
@@ -65,59 +69,42 @@ export default function Packages({
         <>
             <div>
                 {packages.map((data: { [key: string]: any }) => (
-                    <PackageCard key={data.internet_package_id} data={data} className="mb-5" handleButtonClick={handlePackageButtonClick} />
+                    <PackageCard
+                        key={data.internet_package_id}
+                        data={data}
+                        className="mb-5"
+                        handleButtonClick={() => handlePackageButtonClick(data)}
+                    />
                 ))}
             </div>
             <Modal title="Подключение" isOpened={isModalOpened} handleClose={closeModal} ref={modalRef}>
-                <div className="bg-white">
-                    <p> это модально окно</p>
-                    <p> это модально окно</p>
-                    <p> это модально окно</p>
-                    <p> это модально окно</p>
-                    <p> это модально окно</p>
-                    <p> это модально окно</p>
-                    <p> это модально окно</p>
+                <div className="md:flex md:justify-between">
+                    <div className="w-full md:w-5/12 md:order-2">
+                        <div className="bg-white rounded-lg p-4 mb-6">
+                            <p className="text-black text-lg font-bold mb-4">Выбранный пакет:</p>
+                            <Image
+                                src={selectedPackage?.provider_img_url}
+                                alt={`Selected package provider logo - ${selectedPackage?.provider_name}`}
+                                width={50}
+                                height={32}
+                                className="inline mr-3"
+                            />
+                            <p className="inline align-middle">{selectedPackage?.internet_package_name}</p>
+                        </div>
+                        <div className="bg-white rounded-lg p-4 mb-6">
+                            <p className="text-black text-lg font-bold mb-4">Стоимость:</p>
+                            <p className="text-black flex justify-between items-center">
+                                <span className="font-medium">Оплата в месяц:</span>{' '}
+                                <span className="text-2xl font-bold">
+                                    {selectedPackage?.internet_package_price} €
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="w-full md:w-6/12 md:order-1">
+                        <div className="bg-white rounded-lg shadow-md p-4">form</div>
+                    </div>
                 </div>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
-                <p> это модально окно</p>
             </Modal>
         </>
     );
