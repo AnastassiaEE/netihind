1cdcbccab8531f1a3d60e4a8381a3669f1f43782
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-03-02 05:18:46 +0200

    Add CloseButton component.

---
+++ app/components/ui/buttons/CloseButton.tsx
@@ -0,0 +1,17 @@
+import CloseIcon from '@mui/icons-material/Close';
+import classNames from 'classnames';
+
+export default function CloseButton({
+    handleClick,
+    className,
+}: {
+    handleClick: () => void;
+    className?: string;
+}) {
+    const buttonClasses = classNames('rounded-full p-[3px]', className);
+    return (
+        <button type="button" className={buttonClasses} onClick={handleClick}>
+            <CloseIcon className="text-muted transition-colors hover:text-black" />
+        </button>
+    );
+}
