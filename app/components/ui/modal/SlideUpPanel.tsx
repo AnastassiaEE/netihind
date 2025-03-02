c173da39f30616af94e4df821285d25945a7709f
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-03-02 05:24:19 +0200

    Refactor SlideUpPanel to use CloseButton component instead of CloseIcon.

--- app/components/ui/modal/SlideUpPanel.tsx
+++ app/components/ui/modal/SlideUpPanel.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import classNames from 'classnames';
-import CloseIcon from '@mui/icons-material/Close';
 import useSlideUpPanel from '@/hooks/useSlideUpPanel';
 import Backdrop from '@/components/ui/modal/Backdrop';
 import PanelActions from '@/components/ui/modal/PanelActions';
+import CloseButton from '@/components/ui/buttons/CloseButton';
 
 export default function SlideUpPanel({
     title,
@@ -23,7 +23,7 @@ export default function SlideUpPanel({
 
     const panelClasses = classNames(
         'fixed inset-x-0 bottom-0 z-50 h-dvh rounded-t-2xl bg-white transition-transform',
-        isOpened ? 'translate-y-0' : 'translate-y-full pointer-events-none'
+        isOpened ? 'translate-y-0' : 'translate-y-full pointer-events-none',
     );
 
     return (
@@ -32,13 +32,10 @@ export default function SlideUpPanel({
                 <div className={panelClasses} onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd}>
                     <div className="relative border-b border-muted-light px-6 pb-5 pt-7">
                         <p className="text-center text-xl font-extrabold text-black">{title}</p>
-                        <button
-                            type="button"
+                        <CloseButton
+                            handleClick={handleClose}
                             className="absolute right-6 top-1/2 -translate-y-1/2"
-                            onClick={handleClose}
-                        >
-                            <CloseIcon className="text-muted transition-colors hover:text-black" />
-                        </button>
+                        />
                     </div>
                     <div
                         className="overflow-y-auto p-6"
