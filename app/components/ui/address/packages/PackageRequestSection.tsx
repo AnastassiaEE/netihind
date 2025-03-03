e68f6ac70701288aa0532312632fd3b7a1068d24
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-03-03 18:33:48 +0200

    Refactor PackageRequestSection component to improve code readability and increase padding for better layout.

--- app/components/ui/address/packages/PackageRequestSection.tsx
+++ app/components/ui/address/packages/PackageRequestSection.tsx
@@ -1,9 +1,14 @@
-export default function PackageRequestSection({ title, className, children }: { title: string, className?: string, children: React.ReactNode }) {
+export default function PackageRequestSection({
+    title,
+    children,
+}: {
+    title: string;
+    children: React.ReactNode;
+}) {
     return (
-        <div className="bg-white rounded-lg shadow-md p-4">
+        <div className="bg-white rounded-lg shadow-md p-6">
             <p className="text-black text-lg font-bold mb-4">{title}</p>
             {children}
         </div>
-    )
-
+    );
 }
