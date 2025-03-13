13632f0e96d061f6afc8f3e18cb847d175b27a00
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-03-13 05:57:00 +0200

    Refactor LogoCard component to use 'src' and 'alt' props for improved clarity and consistency

--- app/components/ui/logo/LogoCard.tsx
+++ app/components/ui/logo/LogoCard.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image';
+import Image from "next/image";
 
-export default function LogoCard({ image, alt }: { image: string; alt: string }) {
+export default function LogoCard({ src, alt }: { src: string; alt: string }) {
     return (
         <div className="relative h-6 grow duration-500 hover:-translate-y-2 md:h-12">
-            <Image src={image} alt={alt} fill className="object-contain" />
+            <Image src={src} alt={alt} fill className="object-contain" />
         </div>
     );
 }
--- app/components/ui/logo/LogoCards.tsx
+++ app/components/ui/logo/LogoCards.tsx
@@ -3,8 +3,8 @@ import LogoCard from '@/components/ui/logo/LogoCard';
 export default function LogoCards({ logos }: { logos: { [key: string]: string }[] }) {
     return (
         <div className="flex flex-wrap justify-around">
-            {logos.map(({ name, image, alt }) => (
-                <LogoCard key={name} image={image} alt={alt} />
+            {logos.map(({ name, img_src, img_alt }) => (
+                <LogoCard key={name} src={img_src} alt={img_alt} />
             ))}
         </div>
     );
