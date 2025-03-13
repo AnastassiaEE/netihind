4ba99e48d2bbd8b0d02a3495e9af776639e64d1c
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-03-13 07:38:05 +0200

    Remove unused InfoSection and ProvidersLogoSection imports from Home page for code cleanup

--- app/[locale]/(home)/page.tsx
+++ app/[locale]/(home)/page.tsx
@@ -2,8 +2,6 @@ import QuestionsSection from '@/components/sections/home/QuestionsSection';
 import ContactsSection from '@/components/sections/home/ContactsSection';
 import StepsSection from '@/components/sections/home/StepsSection';
 import SliderBlogSection from '@/components/sections/home/SliderBlogSection';
-import InfoSection from '@/components/sections/home/InfoSection';
-import ProvidersLogoSection from '@/components/sections/home/ProvidersLogoSection';
 import TopSection from '@/components/sections/home/TopSection';
 import { getTranslations, setRequestLocale } from 'next-intl/server';
 import { useTranslations } from 'next-intl';
@@ -73,8 +71,6 @@ export default function Home({ params: { locale } }: { params: { locale: string 
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
       />
       <TopSection />
-      {/* <InfoSection i18n={t} /> */}
-      {/* <ProvidersLogoSection /> */}
       <StepsSection />
       <QuestionsSection />
       <SliderBlogSection />
