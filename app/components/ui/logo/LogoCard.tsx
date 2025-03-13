91f5263a0bd708b41453da3b6e5eb5f803053dc9
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-03-13 07:28:57 +0200

    Add 'sizes' attribute to Image component in LogoCard for responsive design

--- app/components/ui/logo/LogoCard.tsx
+++ app/components/ui/logo/LogoCard.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function LogoCard({ src, alt }: { src: string; alt: string }) {
     return (
         <div className="relative h-6 grow duration-500 hover:-translate-y-2 md:h-12">
-            <Image src={src} alt={alt} fill className="object-contain" />
+            <Image src={src} alt={alt} fill sizes="25vw" className="object-contain" />
         </div>
     );
 }
