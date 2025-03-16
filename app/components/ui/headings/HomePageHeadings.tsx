9915718d1e669ae67ce4271817dd63484fb83265
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-03-16 12:39:08 +0200

    Refactor heading components to utilize classNames for improved styling management and consistency.

--- app/components/ui/headings/HomePageHeadings.tsx
+++ app/components/ui/headings/HomePageHeadings.tsx
@@ -1,30 +1,58 @@
 import classNames from 'classnames';
 
-export const H1 = ({ children, className }: { children: React.ReactNode; className?: string }) => {
-    const cs = classNames({ [className as string]: className !== undefined });
-    return (
-        <h1
-            className={`mb-6 text-[calc(1.475rem+2.7vw)] font-extrabold !leading-snug md:text-5xl ${cs}`}
-        >
-            {children}
-        </h1>
-    );
+export const H1 = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <h1
+      className={classNames(
+        'mb-6 text-[calc(1.475rem+2.7vw)] font-extrabold !leading-snug md:text-5xl',
+        className,
+      )}
+    >
+      {children}
+    </h1>
+  );
 };
 
-export const H2 = ({ children, className }: { children: React.ReactNode; className?: string }) => {
-    const cs = classNames({ [className as string]: className !== undefined });
-    return (
-        <h2 className={`mb-6 text-[calc(1.375rem+1.5vw)] font-extrabold md:text-4xl ${cs}`}>
-            {children}
-        </h2>
-    );
+export const H2 = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <h2
+      className={classNames(
+        'mb-6 text-[calc(1.375rem+1.5vw)] font-extrabold md:text-4xl',
+        className,
+      )}
+    >
+      {children}
+    </h2>
+  );
 };
 
-export const H3 = ({ children, className }: { children: React.ReactNode; className?: string }) => {
-    const cs = classNames({ [className as string]: className !== undefined });
-    return (
-        <h3 className={`mb-6 text-[calc(1.325rem+0.9vw)] font-extrabold md:text-3xl ${cs}`}>
-            {children}
-        </h3>
-    );
+export const H3 = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <h3
+      className={classNames(
+        'mb-6 text-[calc(1.325rem+0.9vw)] font-extrabold md:text-3xl',
+        className,
+      )}
+    >
+      {children}
+    </h3>
+  );
 };
--- app/components/ui/headings/RestPageHeadings.tsx
+++ app/components/ui/headings/RestPageHeadings.tsx
@@ -1,21 +1,39 @@
 import classNames from 'classnames';
 
-export const H1 = ({ children, className }: { children: React.ReactNode; className?: string }) => {
-    const cs = classNames({ [className as string]: className !== undefined });
-    return (
-        <h1
-            className={`mb-10 text-[calc(1.375rem+1.5vw)] font-extrabold md:text-4xl md:leading-tight ${cs}`}
-        >
-            {children}
-        </h1>
-    );
+export const H1 = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <h1
+      className={classNames(
+        'mb-10 text-[calc(1.375rem+1.5vw)] font-extrabold md:text-4xl md:leading-tight',
+        className,
+      )}
+    >
+      {children}
+    </h1>
+  );
 };
 
-export const H2 = ({ children, className }: { children: React.ReactNode; className?: string }) => {
-    const cs = classNames({ [className as string]: className !== undefined });
-    return (
-        <h2 className={`mb-6 text-[calc(1.275rem+0.3vw)] font-extrabold md:text-2xl ${cs}`}>
-            {children}
-        </h2>
-    );
+export const H2 = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <h2
+      className={classNames(
+        'mb-6 text-[calc(1.275rem+0.3vw)] font-extrabold md:text-2xl',
+        className,
+      )}
+    >
+      {children}
+    </h2>
+  );
 };
