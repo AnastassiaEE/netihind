4d10e5e55ffbfe7f408cfd47808168eb1f1b8582
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-03-16 07:25:19 +0200

    Refactor Tab component for improved readability and consistency

--- app/components/ui/tabs/Tab.tsx
+++ app/components/ui/tabs/Tab.tsx
@@ -1,33 +1,33 @@
 import classNames from 'classnames';
 
 export default function Tab({
-    tabId,
-    tabPanelId,
-    isActive,
-    handleTabClick,
-    children,
+  tabId,
+  tabPanelId,
+  isActive,
+  handleTabClick,
+  children,
 }: {
-    tabId: string;
-    tabPanelId: string;
-    isActive: boolean;
-    handleTabClick: React.MouseEventHandler<HTMLLIElement>;
-    children: React.ReactNode;
+  tabId: string;
+  tabPanelId: string;
+  isActive: boolean;
+  handleTabClick: React.MouseEventHandler<HTMLLIElement>;
+  children: React.ReactNode;
 }) {
-    const tabClasses = classNames(
-        'flex-1 cursor-pointer p-3 text-center font-semibold',
-        isActive && 'text-primary border-b-2 border-primary',
-    );
-    return (
-        <li
-            id={tabId}
-            role="tab"
-            aria-selected={isActive}
-            aria-controls={tabPanelId}
-            tabIndex={isActive ? 0 : undefined}
-            className={tabClasses}
-            onClick={handleTabClick}
-        >
-            {children}
-        </li>
-    );
+  const tabClasses = classNames(
+    'flex-1 cursor-pointer p-3 text-center font-semibold',
+    isActive && 'border-b-2 border-primary text-primary',
+  );
+  return (
+    <li
+      id={tabId}
+      role="tab"
+      aria-selected={isActive}
+      aria-controls={tabPanelId}
+      tabIndex={isActive ? 0 : undefined}
+      className={tabClasses}
+      onClick={handleTabClick}
+    >
+      {children}
+    </li>
+  );
 }
