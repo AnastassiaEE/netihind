3647ec8b88bc2271a376391b560236b8e76daf38
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-03-16 07:21:42 +0200

    Refactor Textarea component for improved readability and consistency

--- app/components/ui/form/fields/input/Textarea.tsx
+++ app/components/ui/form/fields/input/Textarea.tsx
@@ -4,55 +4,55 @@ import classNames from 'classnames';
 import { FormElementSizes as sizes } from '@/styles/styles';
 
 export default function Textarea({
-    size = 'sm',
-    name,
-    label,
-    placeholder,
-    handleChange,
-    handleBlur,
-    value,
-    isValid = true,
-    error,
-    required,
-    className,
+  size = 'sm',
+  name,
+  label,
+  placeholder,
+  handleChange,
+  handleBlur,
+  value,
+  isValid = true,
+  error,
+  required,
+  className,
 }: {
-    size?: keyof typeof sizes;
-    name: string;
-    label?: string;
-    placeholder?: string;
-    handleChange?: React.ChangeEventHandler<HTMLTextAreaElement>;
-    handleBlur?: React.FocusEventHandler<HTMLTextAreaElement>;
-    value?: string;
-    isValid?: boolean;
-    error?: string;
-    required: boolean;
-    className?: string;
+  size?: keyof typeof sizes;
+  name: string;
+  label?: string;
+  placeholder?: string;
+  handleChange?: React.ChangeEventHandler<HTMLTextAreaElement>;
+  handleBlur?: React.FocusEventHandler<HTMLTextAreaElement>;
+  value?: string;
+  isValid?: boolean;
+  error?: string;
+  required: boolean;
+  className?: string;
 }) {
-    const textAreaClasses = classNames(
-        'placeholder:text-muted w-full rounded-md border bg-white text-muted-dark focus:shadow-lg focus:shadow-indigo-500/10 focus:outline-none',
-        sizes[size],
-        isValid ? 'border-valid focus:border-primary/30' : 'border-invalid',
-        className,
-    );
+  const textAreaClasses = classNames(
+    'w-full rounded-md border bg-white text-muted-dark placeholder:text-muted focus:shadow-lg focus:shadow-indigo-500/10 focus:outline-none',
+    sizes[size],
+    isValid ? 'border-valid focus:border-primary/30' : 'border-invalid',
+    className,
+  );
 
-    return (
-        <>
-            {label && (
-                <FieldLabel htmlFor={name} size={size}>
-                    {label}
-                </FieldLabel>
-            )}
-            <textarea
-                id={name}
-                className={textAreaClasses}
-                placeholder={placeholder}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={value}
-                aria-invalid={!isValid}
-                aria-required={required}
-            ></textarea>
-            {!isValid && <FieldError size={size}>{error}</FieldError>}
-        </>
-    );
+  return (
+    <>
+      {label && (
+        <FieldLabel htmlFor={name} size={size}>
+          {label}
+        </FieldLabel>
+      )}
+      <textarea
+        id={name}
+        className={textAreaClasses}
+        placeholder={placeholder}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        value={value}
+        aria-invalid={!isValid}
+        aria-required={required}
+      ></textarea>
+      {!isValid && <FieldError size={size}>{error}</FieldError>}
+    </>
+  );
 }
