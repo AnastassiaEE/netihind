07714fe7839922b97645e55d4dc781c61a064d48
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-03-16 07:23:16 +0200

    Refactor CircleArrow component for improved readability and consistency

--- app/components/ui/icons/CircleArrow.tsx
+++ app/components/ui/icons/CircleArrow.tsx
@@ -2,20 +2,20 @@ import Arrow, { ArrowDirection } from '@/components/ui/icons/Arrow';
 import classNames from 'classnames';
 
 export default function CircleArrow({
-    direction = 'left',
-    className,
+  direction = 'left',
+  className,
 }: {
-    direction?: ArrowDirection;
-    className?: string;
+  direction?: ArrowDirection;
+  className?: string;
 }) {
-    return (
-        <span
-            className={classNames(
-                'font-semibold flex size-9 shrink-0 cursor-pointer items-center justify-center rounded-full',
-                className,
-            )}
-        >
-            <Arrow direction={direction} />
-        </span>
-    );
+  return (
+    <span
+      className={classNames(
+        'flex size-9 shrink-0 cursor-pointer items-center justify-center rounded-full font-semibold',
+        className,
+      )}
+    >
+      <Arrow direction={direction} />
+    </span>
+  );
 }
