d4c85933325abbbe0f0ee48dfc6880151d8a3717
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-03-16 11:03:16 +0200

    Refactor CookieButton component for improved readability and update button styling for better visibility.

--- app/components/ui/cookies/CookieButton.tsx
+++ app/components/ui/cookies/CookieButton.tsx
@@ -1,15 +1,21 @@
 import CookieIcon from '@mui/icons-material/Cookie';
 
-export default function CookieButton({ label, handleClick }: { label: string, handleClick: () => void }) {
-    return (
-        <button
-            type="button"
-            aria-label={label}
-            onClick={handleClick}
-            className="fixed bottom-2 left-2 z-10 flex w-[50px] items-center overflow-hidden rounded-md bg-primary p-2 font-medium text-white transition-all hover:w-[130px] hover:bg-primary-dark"
-        >
-            <CookieIcon fontSize="large" />
-            <span className="ml-2">Küpsised</span>
-        </button>
-    );
+export default function CookieButton({
+  label,
+  handleClick,
+}: {
+  label: string;
+  handleClick: () => void;
+}) {
+  return (
+    <button
+      type="button"
+      aria-label={label}
+      onClick={handleClick}
+      className="fixed bottom-2 left-2 z-10 flex w-[50px] items-center overflow-hidden rounded-md bg-primary/50 p-2 font-medium text-white transition-all hover:w-[130px] hover:bg-primary-dark"
+    >
+      <CookieIcon fontSize="large" />
+      <span className="ml-2">Küpsised</span>
+    </button>
+  );
 }
