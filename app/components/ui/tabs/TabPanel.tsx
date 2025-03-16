04a4cd29ec039bcb3f84f55462f0e8c8f5d269e4
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-03-16 07:25:55 +0200

    Refactor TabPanel component for improved readability and consistency

--- app/components/ui/tabs/TabPanel.tsx
+++ app/components/ui/tabs/TabPanel.tsx
@@ -1,26 +1,26 @@
 import classNames from 'classnames';
 
 export default function TabPanel({
-    tabId,
-    tabPanelId,
-    isActive,
-    children,
+  tabId,
+  tabPanelId,
+  isActive,
+  children,
 }: {
-    tabId?: string;
-    tabPanelId?: string;
-    isActive?: boolean;
-    children: React.ReactNode;
+  tabId?: string;
+  tabPanelId?: string;
+  isActive?: boolean;
+  children: React.ReactNode;
 }) {
-    const tabPanelClasses = classNames('p-3 w-[700px]', !isActive && 'hidden');
-    return (
-        <div
-            id={tabPanelId}
-            role="tabpanel"
-            aria-labelledby={tabId}
-            tabIndex={isActive ? 0 : undefined}
-            className={tabPanelClasses}
-        >
-            {children}
-        </div>
-    );
+  const tabPanelClasses = classNames('w-[700px] p-3', !isActive && 'hidden');
+  return (
+    <div
+      id={tabPanelId}
+      role="tabpanel"
+      aria-labelledby={tabId}
+      tabIndex={isActive ? 0 : undefined}
+      className={tabPanelClasses}
+    >
+      {children}
+    </div>
+  );
 }
