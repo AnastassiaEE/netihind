04e7b0711f2d22ee15a857e3b97e338a390a0409
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-03-17 03:32:26 +0200

    Refactor Navigation component for improved readability and consistency in JSX structure.

--- app/components/ui/navigation/Navigation.tsx
+++ app/components/ui/navigation/Navigation.tsx
@@ -2,27 +2,28 @@ import React, { isValidElement } from 'react';
 import classNames from 'classnames';
 
 export default function Navigation({
-    type = 'horizontal',
-    className = 'text-muted-dark',
-    children,
+  type = 'horizontal',
+  className = 'text-muted-dark',
+  children,
 }: {
-    type?: 'horizontal' | 'vertical';
-    className?: string;
-    children: React.ReactNode;
+  type?: 'horizontal' | 'vertical';
+  className?: string;
+  children: React.ReactNode;
 }) {
-    const listClasses = classNames('font-semibold', className);
-    const itemClasses = classNames('py-2', type === 'horizontal' && 'inline-block px-4');
-    return (
-        <nav>
-            <ul className={listClasses}>
-                {React.Children.map(children, (child) =>
-                    isValidElement(child) ? (
-                        <li className={itemClasses}>{React.cloneElement(child)}</li>
-                    ) : (
-                        <li className={itemClasses}>{child}</li>
-                    ),
-                )}
-            </ul>
-        </nav>
-    );
+  const listClasses = classNames('font-semibold', className);
+  const itemClasses = classNames(
+    'py-2',
+    type === 'horizontal' && 'inline-block px-4',
+  );
+  return (
+    <nav>
+      <ul className={listClasses}>
+        {React.Children.map(children, (child) => {
+          if (isValidElement(child))
+            return <li className={itemClasses}>{React.cloneElement(child)}</li>;
+          return child;
+        })}
+      </ul>
+    </nav>
+  );
 }
