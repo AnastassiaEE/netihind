3c13c2834993c3a95fdaadfb65b50867f9f475a0
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-03-18 02:41:55 +0200

    Refactor Backdrop component for improved readability and consistent formatting

--- app/components/ui/overlay/Backdrop.tsx
+++ app/components/ui/overlay/Backdrop.tsx
@@ -2,21 +2,24 @@
 import { useEffect } from 'react';
 
 export default function Backdrop({
-    isVisible,
-    handleClose,
+  isVisible,
+  handleClose,
 }: {
-    isVisible: boolean;
-    handleClose: () => void;
+  isVisible: boolean;
+  handleClose: () => void;
 }) {
-    useEffect(() => {
-        document.body.classList.toggle('overflow-hidden', isVisible);
-        return () => document.body.classList.remove('overflow-hidden');
-    }, [isVisible]);
-    return (
-        <>
-            {isVisible && (
-                <div onClick={handleClose} className="fixed inset-0 z-20 cursor-pointer bg-black/50"></div>
-            )}
-        </>
-    );
+  useEffect(() => {
+    document.body.classList.toggle('overflow-hidden', isVisible);
+    return () => document.body.classList.remove('overflow-hidden');
+  }, [isVisible]);
+  return (
+    <>
+      {isVisible && (
+        <div
+          onClick={handleClose}
+          className="fixed inset-0 z-20 cursor-pointer bg-black/50"
+        ></div>
+      )}
+    </>
+  );
 }
