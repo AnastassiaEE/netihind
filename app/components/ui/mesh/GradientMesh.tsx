b996f01527e2b50e7b9ab8099794a7dddf128faf
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-03-19 16:55:46 +0200

    Refactor GradientMesh component to simplify gradient stops and remove animation; enhance readability

--- app/components/ui/mesh/GradientMesh.tsx
+++ app/components/ui/mesh/GradientMesh.tsx
@@ -1,26 +1,19 @@
 import Mesh from '@/components/ui/mesh/Mesh';
 
 const gradientStops = [
-    { offset: '0%', color: '#a5b4fc', animationValues: '#a5b4fc; #ddd6fe; #f5d0fe; #a5b4fc' },
-    { offset: '50%', color: '#ddd6fe', animationValues: '#ddd6fe; #f5d0fe; #a5b4fc; #ddd6fe' },
-    { offset: '100%', color: '#f5d0fe', animationValues: '#f5d0fe; #a5b4fc; #ddd6fe; #f5d0fe' },
+  { offset: '0%', color: '#e7ebff' },
+  { offset: '50%', color: '#e8e3ff' },
+  { offset: '100%', color: '#f9e1ff' },
 ];
 
 export default function GradientMesh() {
-    return (
-        <Mesh>
-            <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
-                {gradientStops.map(({ offset, color, animationValues }) => (
-                    <stop key={offset} offset={offset} stopColor={color}>
-                        <animate
-                            attributeName="stop-color"
-                            values={animationValues}
-                            dur="4s"
-                            repeatCount="indefinite"
-                        />
-                    </stop>
-                ))}
-            </linearGradient>
-        </Mesh>
-    );
+  return (
+    <Mesh>
+      <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+        {gradientStops.map(({ offset, color }) => (
+          <stop key={offset} offset={offset} stopColor={color} />
+        ))}
+      </linearGradient>
+    </Mesh>
+  );
 }
