73d5730c288c4ad7963193d58556ca338003f3a3
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-03-27 01:30:50 +0200

    Added robot.txt and favicon to public folder.

--- middleware.ts
+++ middleware.ts
@@ -64,5 +64,5 @@ export default async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: '/((?!api|_next/static|_next/image|favicon.ico|robots.txt).*)',
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
 };
