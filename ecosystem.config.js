15d9ffe9a3c735dc5ba42cbcf1c3acc8ae859ddd
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-03-26 22:17:46 +0200

    Change gitignore.

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    apps: [
+      {
+        name: 'netihind-test',
+        script: 'npm',
+        args: 'run start',
+        env: {
+          NODE_ENV: 'production',
+          HOST: '127.2.46.244',
+          PORT: 8080,
+        },
+      },
+    ],
+  };
+  
