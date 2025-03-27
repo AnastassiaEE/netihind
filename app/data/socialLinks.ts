8e78a675a7cdf23ac6476a1052a03e7908f575f5
Anastassia Martsenko <martsenkoanastassia56@gmail.com>
2025-03-27 22:45:10 +0200

    Change fb link url.

--- app/data/socialLinks.ts
+++ app/data/socialLinks.ts
@@ -3,7 +3,7 @@ import { Facebook } from '@mui/icons-material';
 const socialLinks = [
   {
     Icon: Facebook,
-    href: 'https://www.facebook.com/share/PUr4KWMKkveH8VBZ/?mibextid=LQQJ4d',
+    href: 'https://www.facebook.com/people/Netihind/61564967082896/',
     label: 'Facebook',
     color: 'hover:bg-facebook-logo hover:shadow-facebook-logo/40',
   },
