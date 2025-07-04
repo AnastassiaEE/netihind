b07a850c3b1ea6e0f903c750ba44e3b2e0d0c6f2
AnastassiaEE <martsenkoanastassia56@gmail.com>
2025-07-04 17:11:31 +0300

    Remove unused imports in PersonalAddress component to clean up code

--- app/[locale]/(rest)/address/[slug]/page.tsx
+++ app/[locale]/(rest)/address/[slug]/page.tsx
@@ -4,8 +4,6 @@ import { notFound } from 'next/navigation';
 import { getAddressCookieValues } from '@/utils/addressCookieHelper';
 import { getAddressSlug } from '@/utils/addressSlugifier';
 import { setRequestLocale } from 'next-intl/server';
-import { Suspense } from 'react';
-import PageLoader from '@/components/ui/loaders/PageLoader';
 import AddressPackagesSection from '@/components/sections/address/AddressPackagesSection';
 
 export default function PersonalAddress({
