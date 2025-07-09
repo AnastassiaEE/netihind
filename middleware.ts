7ef99509248db9ea6abb502f813b7c53ba6c0f41
AnastassiaEE <martsenkoanastassia56@gmail.com>
2025-07-09 22:47:49 +0300

    Change middleware.ts.

--- middleware.ts
+++ middleware.ts
@@ -36,7 +36,8 @@ export default async function middleware(request: NextRequest) {
       'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='
       'sha256-EtMbx9k/muOjUy42QlSBZb6J88TNtSy99Y+VsAia0Mw='
       'sha256-tEd4lBbiysGj/wvNlLDvwZnlGLMloPzWaf2aUrdDBFE='
-      'sha256-ci9xwDutagjdtNCnRmUkQW4727HuaEmJSYCcKZ+Mfyg=';
+      'sha256-ci9xwDutagjdtNCnRmUkQW4727HuaEmJSYCcKZ+Mfyg='
+      'sha256-nGgRbGz9hsufKfu+i0QGgvvWtZBIe2KnFhQalyWW+7o=';
       img-src 'self' data: https://cms.netihind.ee https://rxysmdetqttpdqfmrpym.supabase.co;
       font-src 'self' data:;
       connect-src 'self' https://inaadress.maaamet.ee https://rxysmdetqttpdqfmrpym.supabase.co https://api.resend.com https://region1.google-analytics.com;
