f16edf94651c0a63e82c9fc3e06db32b953d0049
AnastassiaEE <martsenkoanastassia56@gmail.com>
2025-07-28 19:17:33 +0300

    Rename Popover component. Change package card connection button translation.

--- app/components/ui/overlay/Tooltip.tsx
+++ app/components/ui/overlay/Tooltip.tsx
@@ -4,31 +4,38 @@ import useTooltip from '@/hooks/useTooltip';
 import classNames from 'classnames';
 import React from 'react';
 
-export default function Popover({
-    elementToInteract,
-    content,
+export default function Tooltip({
+  elementToInteract,
+  content,
 }: {
-    elementToInteract: React.ReactElement;
-    content: string;
+  elementToInteract: React.ReactElement;
+  content: string;
 }) {
-    const { isVisible, show, hide } = useTooltip();
+  const { isVisible, show, hide } = useTooltip();
 
-    return (
-        <div className="relative">
-            {React.cloneElement(elementToInteract, {
-                onMouseEnter: show,
-                onMouseLeave: hide,
-                onFocus: show,
-                onBlur: hide,
-                className: classNames(elementToInteract.props.className, 'cursor-pointer text-xs'),
-                'aria-describedby': isVisible ? 'tooltip-content' : undefined,
-                'tabIndex': 0
-            })}
-            {isVisible && (
-                <span id="tooltip-content" role="tooltip" className="absolute -left-full bottom-full w-max max-w-xs rounded-lg bg-white p-3 text-center text-sm lowercase shadow-md">
-                    {content}
-                </span>
-            )}
-        </div>
-    );
+  return (
+    <div className="relative">
+      {React.cloneElement(elementToInteract, {
+        onMouseEnter: show,
+        onMouseLeave: hide,
+        onFocus: show,
+        onBlur: hide,
+        className: classNames(
+          elementToInteract.props.className,
+          'cursor-pointer text-xs',
+        ),
+        'aria-describedby': isVisible ? 'tooltip-content' : undefined,
+        tabIndex: 0,
+      })}
+      {isVisible && (
+        <span
+          id="tooltip-content"
+          role="tooltip"
+          className="absolute -left-full bottom-full w-max max-w-xs rounded-lg bg-white p-3 text-center text-sm lowercase shadow-md"
+        >
+          {content}
+        </span>
+      )}
+    </div>
+  );
 }
