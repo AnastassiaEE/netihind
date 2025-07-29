d3c0577115504f9eb38051a6fcf504ba374b40c2
AnastassiaEE <martsenkoanastassia56@gmail.com>
2025-07-29 22:36:38 +0300

    Enable the ignoreDuringBuilds option in the eslint config.

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,9 @@ const nextConfig = {
   //output: 'export',
   reactStrictMode: true,
   trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
