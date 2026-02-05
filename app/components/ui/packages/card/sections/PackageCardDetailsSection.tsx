282ec1a56ed1510ced94018084fd81118af47800
AnastassiaEE <martsenkoanastassia56@gmail.com>
2026-02-05 19:56:36 +0200

    fix: adjust layout spacing in PackageCardDetailsSection for improved responsiveness

--- app/components/ui/packages/card/sections/PackageCardDetailsSection.tsx
+++ app/components/ui/packages/card/sections/PackageCardDetailsSection.tsx
@@ -23,7 +23,7 @@ export default function PackageCardDetailsSection({
 
   return (
     <div className={className}>
-      <div className="flex flex-wrap gap-3">
+      <div className="flex flex-wrap gap-x-3 gap-y-1">
         {installation && (
           <PackageCardDetail Icon={Engineering}>
             <>
