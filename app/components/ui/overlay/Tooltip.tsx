caac304a7cd3a5e2c5814b8ba95adf12e89b59aa
AnastassiaEE <martsenkoanastassia56@gmail.com>
2026-02-11 19:15:55 +0200

    refactor: simplify Tooltip component by using usePortal for tooltip rendering

--- app/components/ui/overlay/Tooltip.tsx
+++ app/components/ui/overlay/Tooltip.tsx
@@ -1,15 +1,31 @@
+import { usePortal } from '@/hooks/usePortal';
 import useTooltip from '@/hooks/useTooltip';
 import React from 'react';
-import { createPortal } from 'react-dom';
 
 export default function Tooltip({
   elementToInteract,
   content,
 }: {
-  elementToInteract: React.ReactElement<any>;
+  elementToInteract: React.ReactNode;
   content: string;
 }) {
   const { isVisible, pos, wrapperRef, tooltipRef, show, hide } = useTooltip();
+
+  const tooltipPortal = usePortal(
+    <div
+      id="tooltip-content"
+      ref={tooltipRef}
+      className="fixed z-50 max-w-[250px] rounded-md bg-white p-3 text-sm text-muted-dark shadow-md"
+      style={{
+        top: pos.top,
+        left: pos.left,
+        transform: 'translateX(-50%)',
+      }}
+    >
+      {content}
+    </div>,
+    isVisible,
+  );
 
   return (
     <div
@@ -23,22 +39,7 @@ export default function Tooltip({
       className="relative inline-block cursor-pointer"
     >
       {elementToInteract}
-
-      {isVisible &&
-        createPortal(
-          <div
-            ref={tooltipRef}
-            className="fixed z-50 max-w-[250px] rounded-md bg-white p-3 text-sm text-muted-dark shadow-md"
-            style={{
-              top: pos.top,
-              left: pos.left,
-              transform: 'translateX(-50%)',
-            }}
-          >
-            {content}
-          </div>,
-          document.body,
-        )}
+      {tooltipPortal}
     </div>
   );
 }
