5ac809df9211e16c68a88a649b138fc17afaf5bb
AnastassiaEE <martsenkoanastassia56@gmail.com>
2026-02-18 17:36:40 +0200

    fix: adjust background accent colors for improved visual consistency

--- app/components/sections/home/TopSection.tsx
+++ app/components/sections/home/TopSection.tsx
@@ -11,7 +11,7 @@ export default function TopSection() {
   return (
     <SectionLayout
       className="relative flex h-screen min-h-162.5 flex-col justify-center pt-20 md:min-h-180 md:pt-35"
-      bg="bg-linear-to-t from-primary/20 from-0% via-accent/10 via-secondary/10 via-40% to-80%"
+      bg="bg-linear-to-t from-primary/20 from-0% via-accent/20 via-secondary/20 via-40% to-80%"
     >
       <div className="absolute right-0 bottom-12.5 -z-10 h-auto w-57.5 max-md:top-20 md:w-125">
         <GradientMesh />
