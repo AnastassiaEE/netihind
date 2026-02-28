e231dab81d47b5368d89dd94501d06e7b4acdcfd
AnastassiaEE <martsenkoanastassia56@gmail.com>
2026-02-28 16:50:38 +0200

    refactor: update JSDoc comments for translateKey function to clarify parameter and return value

--- app/utils/translationHelper.ts
+++ app/utils/translationHelper.ts
@@ -4,9 +4,9 @@ import { TranslationFn } from '@/types/translations.types';
  * Translates a dynamic key safely using next-intl.
  *
  * @param t - Translation function
- * @param key - Translation key (optional). Returns '' if undefined
+ * @param key - Translation key (optional).
  *
- * @returns Translated string or empty string
+ * @returns Translated string or empty string if key is undefined
  */
 export const translateKey = (t: TranslationFn, key?: string) => {
   return key ? t(key as Parameters<typeof t>[0]) : '';
